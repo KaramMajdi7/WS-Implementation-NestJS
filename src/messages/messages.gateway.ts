d6d9d1a37c06cde573870105931b4c268d06e081
Karam Majdi <150668777+Karam-Majdi@users.noreply.github.com>
2023-12-07 17:59:30 +0300

    Before Apache

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -24,9 +24,9 @@ export class MessagesGateway implements OnGatewayInit, OnGatewayConnection, OnGa
   }
 
   @SubscribeMessage('chatToServer')
-  handleMessage(client: Socket, message: { name: string, message: string }) {
-    this.server.emit('chatToClient', message)
-    this.logger.log(`Client connected: ${client.id} | ${message.name}: Message ${message.message}`);
+  handleMessage(client: Socket, message: { name: string, message: string, room: string }) {
+    this.server.to(message.room).emit('chatToClient', message)
+    this.logger.log(`Client connected: ${client.id} | ${message.name}: Message ${message.message} to room ${message.room}`);
   }
 
   @SubscribeMessage('join')
@@ -40,5 +40,19 @@ export class MessagesGateway implements OnGatewayInit, OnGatewayConnection, OnGa
     const name = this.messagesService.getClientName(client.id);
     this.logger.log(`Client connected: ${client.id}: Message ${isTyping}`);
     client.broadcast.emit('typing', { name, isTyping });
+  }
+
+  @SubscribeMessage('joinRoom')
+  RoomIn(client: Socket, room: string) {
+    client.join(room)
+    this.logger.log(`Client with ID: ${client.id} joined room [${room}]`)
+    client.emit('joinedRoom', room)
+  }
+
+  @SubscribeMessage('leaveRoom')
+  RoomOut(client: Socket, room: string) {
+    client.leave(room)
+    this.logger.log(`Client with ID: ${client.id} left room [${room}]`)
+    client.emit('leftRoom', room)
   }
 }
